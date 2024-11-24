69336ee78a8c83b2b8bdf82ca91db6574e339f97
mohamadreza <mohamadreza1388.org@gmail.com>
2024-11-24 18:30:18 +0330

    init()

---
+++ README.md
@@ -0,0 +1,9 @@
+#foodMenuBack
+
+backend and api for food front project
+
+##for start
+
+```shell
+npm i && npm run dev && composer i && composer run dev
+```
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+
+    ],
+    theme: {
+        extend: {
+
+        },
+    },
+    plugins: [],
+};
